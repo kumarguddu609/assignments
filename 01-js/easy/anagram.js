4940c8306f1ecf2dae5624c428b522ea8900e432
Guddu Kumar <kumarguddu609@gmail.com>
2023-12-26 13:13:44 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,20 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if(str1.length != str2.length){
+    return false;
+  }
+  else{
+    let str1n = str1.split('').sort().join('');
+    let str2n = str2.split('').sort().join('');
+    if(str1n==str2n){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
 }
+console.log(isAnagram("hello","olleo"));
 
 module.exports = isAnagram;
